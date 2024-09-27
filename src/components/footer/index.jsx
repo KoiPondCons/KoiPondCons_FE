792a421715056b4364eed4e6a1d38c11a7fcaa33
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-09-27 20:58:12 +0700

    Add icons to footer for better navigation

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import "./index.css";
+import {
+  FaWrench,
+  FaUserGroup,
+  FaPhone,
+  FaSuitcase,
+  FaEnvelope,
+} from "react-icons/fa6";
+import { IoHomeSharp } from "react-icons/io5";
+
 const Footer = () => {
   const address = "Đường  D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, Hồ Chí Minh";
   const factoryAddress = "cái này ghi gì??";
@@ -7,52 +16,73 @@ const Footer = () => {
   const fax = "0909090909";
   const email = "koinhathang@gmail.com";
   const codeBusiness = "hong bik ghi gì";
-  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.";
+  const description =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.";
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-contact">
           <h2>Công ty TNHH Thiết kế Thi công Koi Team</h2>
           <ul>
-            <li>Văn phòng: {address}</li>
-            <li>Xưởng: {factoryAddress}</li>
-            <li>Điện thoại: {phone} </li>
-            <li>Số Fax: {fax} </li>
-            <li>Email: {email} </li>
-            <li>Mã số doanh nghiệp: {codeBusiness} </li>
+            <li>
+              <IoHomeSharp />
+              Văn phòng: {address}
+            </li>
+            <li>
+              <FaWrench />
+              Xưởng: {factoryAddress}
+            </li>
+            <li>
+              <FaPhone />
+              Điện thoại: {phone}{" "}
+            </li>
+            <li>
+              <FaSuitcase />
+              Số Fax: {fax}{" "}
+            </li>
+            <li>
+              <FaEnvelope />
+              Email: {email}{" "}
+            </li>
+            <li>
+              <FaUserGroup />
+              Mã số doanh nghiệp: {codeBusiness}{" "}
+            </li>
           </ul>
         </div>
         <hr className="footer__hr--vertical"></hr>
         <div className="footer-nav">
           <ul>
             <li>
-              <a href="#homepage">Trang chủ</a>
+              <a href="homepage">Trang chủ</a>
             </li>
             <li>
-              <a href="#about">Giới thiệu</a>
+              <a href="about">Giới thiệu</a>
             </li>
             <li>
-              <a href="#projects">Dự án</a>
+              <a href="projects">Dự án</a>
             </li>
             <li>
-              <a href="#services">Dịch vụ</a>
+              <a href="services">Dịch vụ</a>
             </li>
             <li>
-              <a href="#pricing">Báo giá</a>
+              <a href="pricing">Báo giá</a>
             </li>
             <li>
-              <a href="#blog">Blog</a>
+              <a href="blog">Blog</a>
             </li>
             <li>
-              <a href="#contact">Liên hệ</a>
+              <a href="contact">Liên hệ</a>
             </li>
           </ul>
         </div>
         <hr className="footer__hr--vertical"></hr>
         <div className="footer-title">
-          <h2 className="footer-title__text footer-title__text--underline">BÁO GIÁ</h2>
+          <h2 className="footer-title__text footer-title__text--underline">
+            BÁO GIÁ
+          </h2>
           <h2 className="footer-title__text">NHẬN TƯ VẤN</h2>
-          <p  className="footer-description">{description}</p>
+          <p className="footer-description">{description}</p>
         </div>
       </div>
       <div className="footer-coppyright">
