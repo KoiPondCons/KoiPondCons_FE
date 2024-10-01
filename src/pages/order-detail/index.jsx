28046968688f97295357079c1459f906376c8674
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-01 22:01:38 +0700

    reset page orderdetail

--- src/pages/order-detail/index.jsx
+++ src/pages/order-detail/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { Col, Form, Input, Row, Select } from "antd";
+import FormItem from "antd/es/form/FormItem";
 function OrderDetail() {
-  return <div>OrderDetail</div>;
+  return (
+    <div>
+      <h1>Thông tin tư vấn khách hàng</h1>
+      <Row></Row>
+    </div>
+  );
 }
 
 export default OrderDetail;
