49ea65e1adb7207378acd13ca361c7d847b5ae67
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-01 23:54:57 +0700

    I write this for consulting not manager, but i think it useful so i keep it

--- src/pages/order-detail/index.jsx
+++ src/pages/order-detail/index.jsx
@@ -1,12 +1,55 @@
+import { Col, Form, Input, Row } from "antd";
+import FormItem from "antd/es/form/FormItem";
 import React from "react";
-import { Row } from "antd";
-function OrderDetail() {
+import "./index.css";
+function Order() {
   return (
     <div>
-      <h1>Thông tin tư vấn khách hàng</h1>
-      <Row></Row>
+      <h1>THÔNG TIN ĐƠN HÀNG</h1>
+      <Form layout="vertical">
+        <Row gutter={24}>
+          <Col span={8}>
+            <FormItem label={"Họ tên"}>
+              <Input value={"Anh Khoa"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={8}>
+            <FormItem label={"Số điện thoại"}>
+              <Input value={"0931445958"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={8}>
+            <FormItem label={"Email"}>
+              <Input value={"phamkhoa999@gmail.com"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={24}>
+            <FormItem label={"Địa chỉ"}>
+              <Input value={"15 Long Hưng, P.7, Q.Tân Bình"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={10}>
+            <FormItem label={"Trạng thái"}>
+              <Input value={"Designed"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={7}>
+            <FormItem label={"Gói"}>
+              <Input value={"Thường"} readOnly />
+            </FormItem>
+          </Col>
+          <Col span={7}>
+            <FormItem label={"Thể tích hồ"}>
+              <Input value={"20m3"} readOnly />
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+      <h1>BẢNG BÁO GIÁ</h1>
+      <div></div>
+      <h1>DỰ ÁN THI CÔNG</h1>
     </div>
   );
 }
 
-export default OrderDetail;
+export default Order;
