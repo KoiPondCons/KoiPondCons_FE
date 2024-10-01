67207093cdc45097efd2dd70a9a1d2086eee985f
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-01 23:53:59 +0700

    update path router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ConsultationRequests from "./pages/consultation-requests";
 import MyProfile from "./pages/my-profile";
-import CreatedOrders from "./pages/created-orders";
-import OngoingConsultations from "./pages/ongoing-consultations";
-import OrderDetail from "./pages/order-detail";
+import Order from "./pages/order-detail";
+import OrderManagement from "./pages/manage-order";
 function App() {
   const router = createBrowserRouter([
     {
       path: "",
-      element: <ConsultationRequests />,
+      element: <OrderManagement />,
     },
     {
-      path: "consultation-requests",
-      element: <ConsultationRequests />,
-    },
-    {
-      path: "created-orders",
-      element: <CreatedOrders />,
-    },
-    {
-      path: "ongoing-consultation",
-      element: <OngoingConsultations />,
+      path: "order-management",
+      element: <OrderManagement />,
     },
     {
       path: "my-profile",
       element: <MyProfile />,
     },
     {
-      path: "order-detail",
-      element: <OrderDetail />,
+      path: "order",
+      element: <Order />,
     },
   ]);
 
