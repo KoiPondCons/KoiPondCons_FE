819cf917c289a3f11cf4b860bdb1c513d3aac2a4
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-02 16:09:20 +0700

    Fix title and banner

--- src/components/common-page-template/index.jsx
+++ src/components/common-page-template/index.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import "./index.css";
 import Header from "../header";
 import Footer from "../footer";
-const CommonPageTemplate = ({ children }) => {
-  const title = "Liên hệ";
-  const banner =
-    "https://images.unsplash.com/photo-1588171562256-e0a7313153f4?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const CommonPageTemplate = ({ children, title, banner }) => {
   return (
     <div>
       <Header />
