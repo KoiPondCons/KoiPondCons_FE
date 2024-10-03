7b1b681292f1cdfd09cfddf03fb7fc30a2263441
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-03 14:17:15 +0700

    Update icon user (include dropdown)

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
+import { FaRegUser } from "react-icons/fa";
+import { Select } from "antd";
 
 
 function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -29,17 +41,38 @@ function Header() {
             <a href="/contact">LIÊN HỆ</a>
           </li>
         </ul>
-        <div className="navbar-auth">
-          <a href="register" className="auth-button">
-            Đăng ký
-          </a>
-          <a
-            href="login"
-            className="auth-button"
-            style={{ paddingLeft: "0px" }}
-          >
-            Đăng nhập
-          </a>
+        <div className="navbar-auth"> { isLoggedIn ? 
+        (
+          <>
+           <a href="register" className="auth-button">
+           Đăng ký
+         </a>
+         <a
+           href="login"
+           className="auth-button"
+           style={{ paddingLeft: "0px" }}
+         >
+           Đăng nhập
+         </a>
+         </>
+        )
+          : (
+            <>
+              <FaRegUser onClick={toggleDropdown} style={{cursor: "pointer"}} />
+              {isDropdownOpen && (
+                <div className="dropdown-menu">
+                  <ul>
+                <li><a href="/profile">Hồ sơ cá nhân</a></li>
+                <li><a href="/history">Lịch sử</a></li>
+                <li><a href="/promotion">Khuyến mãi tích điểm</a></li>
+                <li>Đăng xuất onClick={handleLogout}</li>
+              </ul>
+                </div>
+              )}
+
+            </>
+          )
+        }
         </div>
       </div>
     </nav>
