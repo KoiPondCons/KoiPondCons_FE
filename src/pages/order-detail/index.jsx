7e766a3cade08bd6f144f18d5f11381cb2a3e05b
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-03 22:53:13 +0700

    Moving OrderDetail, also update button file design and priceList

--- src/pages/order-detail/index.jsx
+++ src/pages/order-detail/index.jsx
@@ -1,102 +1,232 @@
-import React, { useState } from 'react';
-import { Input, Button, Form } from 'antd';
-import { FaChevronLeft } from 'react-icons/fa';
-import './index.css'; // Đảm bảo bạn đã tạo file CSS để tùy chỉnh
+import { Col, Form, Input, Popover, Row, Table } from "antd";
+import FormItem from "antd/es/form/FormItem";
+import React, { useEffect, useState } from "react";
+import { Progress } from "antd";
+import axios from "axios";
+import "./index.css";
+import NavDashboard from "../../components/navbar-dashboard";
+import { useParams } from "react-router-dom";
+import "../../components/table/index.css";
+import { AiOutlineFile } from "react-icons/ai";
+import { RiDraftLine } from "react-icons/ri";
+function Order() {
+  const { id } = useParams();
+  const [constructionOrder, setConstructionOrder] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchConstructionOrder = async () => {
+      try {
+        const response = await axios.get(
+          `https://66fa4cd2afc569e13a9b1aed.mockapi.io/ConstructionOrder/${id}`
+        );
+        setConstructionOrder(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-const ConsultationPage = () => {
-  const [formValues, setFormValues] = useState({
-    name: "Phạm Nguyên Khoa",
-    phone: "082 624 8258",
-    email: "phamkhoa999@gmail.com",
-    service: "Khác",
-    address: "32/18 Nguyễn Nhữ Lâm, P.Phú Thọ Hòa, Q. Tân Phú",
-    volume: "8 m³",
-    package: "Khác",
-    consultationContent: "Sao chưa bảo dưỡng định kỳ cho mình vậy?"
-  });
+    fetchConstructionOrder();
+  }, [id]);
+ 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching data: {error.message}</div>;
+  const dataSource = [
+    {
+      key: "1",
+      installment: "Chi phí xây",
+      percentage: "",
+      amount: 3300000,
+      status: "",
+      remaining: "", // Tổng số tiền còn lại
+    },
+    {
+      key: "2",
+      installment: "Giảm Giá",
+      percentage: "",
+      amount: -300000,
+      status: "",
+      remaining: "",
+    },
+    {
+      key: "3",
+      installment: "Tổng Sau Giảm",
+      percentage: "",
+      amount: 3000000,
+      status: "",
+      remaining: "",
+    },
+    {
+      key: "4",
+      installment: "Đợt 1",
+      percentage: "20%",
+      amount: 600000,
+      status: "Đã Thanh Toán",
+      remaining: 0,
+    },
+    {
+      key: "5",
+      installment: "Đợt 2",
+      percentage: "30%",
+      amount: 900000,
+      status: "Chưa Thanh Toán",
+      remaining: 900000,
+    },
+    {
+      key: "6",
+      installment: "Đợt 3",
+      percentage: "50%",
+      amount: 1500000,
+      status: "Chưa Thanh Toán",
+      remaining: 1500000,
+    },
+  ];
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+  const columns = [
+    {
+      title: "Đợt Thanh Toán",
+      dataIndex: "installment",
+      key: "installment",
+    },
+    {
+      title: "Tỷ Lệ (%)",
+      dataIndex: "percentage",
+      key: "percentage",
+    },
+    {
+      title: "Số Tiền (VND)",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text) => text.toLocaleString("vi-VN"), // Định dạng số tiền
+    },
+    {
+      title: "Tình Trạng",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "Còn Lại (VND)",
+      dataIndex: "remaining",
+      key: "remaining",
+      render: (text) => (text ? text.toLocaleString("vi-VN") : ""), // Định dạng số tiền
+    },
+  ];
 
+ 
   return (
-    <div className="consultation-container">
-      <div className="navigation">
-        <Button icon={<FaChevronLeft />} />
+    <NavDashboard>
+      <div>
+        <h1>THÔNG TIN ĐƠN HÀNG</h1>
+        <Form layout="vertical">
+          <Row gutter={24}>
+            <Col span={8}>
+            <label>Họ tên</label>
+              <div className="display-input">
+                <span> {constructionOrder.customer_name}</span>
+              </div>
+              
+            </Col>
+            <Col span={8}>
+            <label>Số điện thoại</label>
+              <div className="display-input">
+                <span> {constructionOrder.customer_phone}</span>
+              </div>
+             
+            </Col>
+            <Col span={8}>
+            <label>Email</label>
+              <div className="display-input">
+                <span> {constructionOrder.customer_email}</span>
+              </div>
+            
+            </Col>
+            <Col span={24}>
+            <label>Địa chỉ thi công</label>
+              <div className="display-input">
+                <span> {constructionOrder.pond_address}</span>
+              </div>
+            </Col>
+            <Col span={10}>
+            <label>Trạng thái</label>
+              <div className="display-input">
+                <span> {constructionOrder.status}</span>
+              </div>
+              
+            </Col>
+            <Col span={7}>
+            <label>Gói</label>
+              <div className="display-input">
+                  <span> Trần Kim Nhã</span>
+              </div>
+              
+            </Col>
+            <Col span={7}>
+            <label>Thể tích hồ</label>
+              <div className="display-input">
+                <span> {constructionOrder.pond_volume}</span>
+              </div>
+             
+            </Col>
+          </Row>
+        </Form>
+        <h1>ĐẢM NHẬN VÀ TIẾN ĐỘ THI CÔNG</h1>
+        <Form layout="vertical">
+          <Row gutter={24}>
+            <Col span={4}>
+              <div className="progress">
+                <Progress type="circle" percent={80} />
+              </div>
+              <h3 style={{ textAlign: "center", margin: " 20px 20px 0px 20px" }}>Tiến độ</h3>
+              <a className="more-detail" href="">
+                Xem chi tiết
+              </a>
+            </Col>
+            <Col span={20}>
+            <label>Tư vấn viên</label>
+              <div className="display-input">
+                <span> Trần Kim Nhã</span>
+              </div>
+              <label>Nhà thiết kế</label>
+              <div className="display-input">
+                <span> Trần Kim Nhã</span>
+              </div>
+              <label>Chịu trách nhiệm thi công</label>
+              <div className="display-input">
+                <span> Trần Kim Nhã</span>
+              </div>
+            </Col>
+            <Col span={8}>
+            <label>Ngày tiếp nhận</label>
+              <div className="display-input">
+                <span>{constructionOrder.request_date}</span>
+              </div>
+            </Col>
+            <Col span={8}>
+              <label>Bản vẽ thiết kế</label>
+              <div className="display-input">
+                {<RiDraftLine style={{fontSize: "20px"}}/>}
+              </div>
+            </Col>
+            <Col span={8}>
+              <label>Bảng báo giá và chi tiết hạng mục</label>
+              <div className="display-input">
+                {<AiOutlineFile style={{fontSize: "20px", textAlign: "center"}}/>}
+              </div>
+            </Col>
+          </Row>
+        </Form>
+        
+        <h1>Báo giá</h1>
+        <Table
+          className="table-template"
+          dataSource={dataSource}
+          columns={columns}
+          pagination={false}
+        />
       </div>
-      <h1>THÔNG TIN TƯ VẤN KHÁCH HÀNG</h1>
-      <Form layout="vertical">
-        <div className="form-row">
-          <Form.Item label="Họ tên" name="name" className="form-item">
-            <Input 
-              name="name" 
-              value={formValues.name} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-          <Form.Item label="Số điện thoại" name="phone" className="form-item">
-            <Input 
-              name="phone" 
-              value={formValues.phone} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-        </div>
-        <div className="form-row">
-          <Form.Item label="Email" name="email" className="form-item">
-            <Input 
-              name="email" 
-              value={formValues.email} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-          <Form.Item label="Dịch vụ" name="service" className="form-item">
-            <Input 
-              name="service" 
-              value={formValues.service} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-        </div>
-        <div className="form-row">
-          <Form.Item label="Địa chỉ" name="address" className="form-item">
-            <Input 
-              name="address" 
-              value={formValues.address} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-          <Form.Item label="Thể tích hồ" name="volume" className="form-item">
-            <Input 
-              name="volume" 
-              value={formValues.volume} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-        </div>
-        <div className="form-row">
-          <Form.Item label="Gói" name="package" className="form-item">
-            <Input 
-              name="package" 
-              value={formValues.package} 
-              onChange={handleChange} 
-            />
-          </Form.Item>
-        </div>
-        <Form.Item label="Nội dung tư vấn" name="consultationContent">
-          <Input.TextArea 
-            name="consultationContent" 
-            value={formValues.consultationContent} 
-            onChange={handleChange} 
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">Gửi</Button>
-        </Form.Item>
-      </Form>
-    </div>
+    </NavDashboard>
   );
-};
-
-export default ConsultationPage;
+}
+export default Order;
