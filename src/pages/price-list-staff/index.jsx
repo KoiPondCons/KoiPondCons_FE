816fdbdf1bcb14a105eafe8bf2d391c8307b13c6
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-06 01:51:15 +0700

    create table send price and css

--- src/pages/price-list-staff/index.jsx
+++ src/pages/price-list-staff/index.jsx
@@ -8,7 +8,7 @@ function PriceListStaff() {
   const [packageItems, setPackageItems] = useState([]);
   const [unitPrice, setUnitPrice] = useState(100000);
   const [discount, setDiscount] = useState(5);
-  const contentdiscount = "Sử dụng thiết kế mẫu giảm("+discount+"%)";
+  const contentdiscount = "Sử dụng thiết kế mẫu giảm(" + discount + "%)";
   const apiPackage =
     "https://66fa4cd2afc569e13a9b1aed.mockapi.io/PackageConstructionItem";
   useEffect(() => {
@@ -54,69 +54,95 @@ function PriceListStaff() {
   ];
   return (
     <div className="price-list-staff-container">
-        <NavDashboard>
-          <h1>Bảng báo giá và chi tiết hạng mục</h1>
-          <div className="price-list-staff-form">
+      <NavDashboard>
+        {/* Bảng báo giá và chi tiết hạng mục */}
+        <h1>Bảng báo giá và chi tiết hạng mục</h1>
+        <div className="price-list-staff-form">
           <Form layout="vertical">
             <Row gutter={40}>
               <Col span={8}>
                 <FormItem label="Thể tích hồ"
-                key="pond-volume">
-                  <Input type="number" defaultValue={pondVolume} 
-                  addonAfter="m3"
-                  min={0}
-                  max={1000}
-                  onChange={(e) => setPondVolume(e.target.value)}
+                  key="pond-volume">
+                  <Input type="number" defaultValue={pondVolume}
+                    addonAfter="m3"
+                    min={0}
+                    max={1000}
+                    onChange={(e) => setPondVolume(e.target.value)}
                   />
                 </FormItem>
               </Col>
               <Col span={8}>
-              <FormItem label="Gói"
-            key="packages"
-            rules={[
-              {
-                required: true,
-                message: "Vui lòng chọn gói!",
-              },
-            ]}
-            >
-              <Select placeholder="Chọn gói">
-                <Select.Option value="1">Gói cơ bản</Select.Option>
-                <Select.Option value="2">Gói phổ thông  </Select.Option>
-                <Select.Option value="3">Gói cao cấp</Select.Option>
-              </Select>
-            </FormItem>
+                <FormItem label="Gói"
+                  key="packages"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng chọn gói!",
+                    },
+                  ]}
+                >
+                  <Select placeholder="Chọn gói">
+                    <Select.Option value="1">Gói cơ bản</Select.Option>
+                    <Select.Option value="2">Gói phổ thông  </Select.Option>
+                    <Select.Option value="3">Gói cao cấp</Select.Option>
+                  </Select>
+                </FormItem>
               </Col>
               <Col span={8}>
-                <Button type="primary" htmlType="submit" style={{position: "absolute", bottom: "15%", width: "90%"}}>
+                <Button type="primary" htmlType="submit" style={{ position: "absolute", bottom: "15%", width: "90%" }}>
                   Tính toán
                 </Button>
               </Col>
             </Row>
           </Form>
-
-            <div className="price-list-staff-result">
-          <h1>Chi tiết hạng mục</h1>
-        <Table
-          className="table-template"
-          dataSource={clonedData}
-          columns={columnsPackage}
-          pagination={false}
-          />
+          {/* Chi tiết hạng mục */}
+          <div className="price-list-staff-result">
+            <h1>Chi tiết hạng mục</h1>
+            <Table
+              className="table-template"
+              dataSource={clonedData}
+              columns={columnsPackage}
+              pagination={false}
+            />
           </div>
-          <h1 style={{textAlign: "center"}}>Báo giá</h1>
-          <h3>Đơn giá: {unitPrice} VNĐ/m3</h3>
-          <h3>Thành tiền: {unitPrice * pondVolume} VNĐ</h3>
-          <h3>Giảm giá:</h3>
-          <span>{contentdiscount}: {unitPrice * pondVolume * discount/100} VNĐ</span>
-          <h3>Tổng cộng: {unitPrice * pondVolume * (1-discount/100)} VNĐ</h3>
+          {/* Báo giá */}
+          <div class="container">
+          <div class="card cart">
+            <label class="title">BÁO GIÁ</label>
+            <div class="steps">
+              <div class="step">
+                <div class="payments">
+                  <p>ƯỚC LƯỢNG</p>
+                  <div class="details">
+                  <span style={{fontWeight:'bold'}}>Đơn giá</span>
+                    <span style={{textAlign:'right'}}>100.000 VND/m3</span>
+                    <span style={{fontWeight:'bold'}}>Thể tích</span>
+                    <span style={{textAlign:'right'}}>100 m3</span>
+                  </div>
+                  <hr />
+                  <p style={{marginTop:'20px'}}>THANH TOÁN</p>
+                  <div class="details">
+                    <span style={{fontWeight:'bold'}}>Thành tiền</span>
+                    <span style={{textAlign:'right'}}>100.000.000 VND</span>
+                    <span style={{fontWeight:'bold'}}>Giảm giá</span>
+                    <span style={{textAlign:'right'}}>5%</span>
+                    <span style={{fontWeight:'bold'}}></span>
+                    <span style={{textAlign:'right',gap:'1px'}}>5.000.000 VND</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card checkout">
+            <div class="footer">
+              <label class="price">95.000.000 VND</label>
+              <button  class="checkout-btn">Gửi báo giá</button>
+            </div>
+          </div>
+        </div>
         </div>
         
-      <Button className="button-template" type="primary" htmlType="submit" style={{textAlign: "center", width: "100%"}}>
-            Gửi báo giá
-        </Button>
-        </NavDashboard>
-        
+      </NavDashboard>
     </div>
   );
 }
