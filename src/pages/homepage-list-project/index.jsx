ce134b578e32fe3083f147a97e16fefdf86b2c0c
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-05 23:12:45 +0700

    create list page

---
+++ src/pages/homepage-list-project/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+function ListProject() {
+  return (
+    <div>
+        <Header />
+        <div>
+            {/* nội dung ở đây */}
+
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default ListProject
