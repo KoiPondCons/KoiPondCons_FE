0e8b65f74b23363564654f5f105b70d01186c687
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-05 23:09:31 +0700

    fix css and inset new tittle

--- src/components/common-page-template/index.jsx
+++ src/components/common-page-template/index.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./index.css";
 import Header from "../header";
 import Footer from "../footer";
-const CommonPageTemplate = ({ children, title, banner }) => {
+const CommonPageTemplate = ({ children, context, title, banner }) => {
   return (
     <div>
       <Header />
       <div className="common-page-container">
         <div className="common-page-banner">
           <div className="banner-title-container">
-            <h1 className="banner-title">{title}</h1>
+            <div className="banner-text">
+              <h1 className="banner-title">{title}</h1>
+              <p className="banner-context">{context}</p>
+            </div>
           </div>
           <img src={banner} alt="" />
         </div>
