cf834dbfcf3b6534978ea5423d1ed276bb055d49
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-05 23:13:29 +0700

    create service page

---
+++ src/pages/homepage-service/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+function Service() {
+  return (
+    <div>
+        <Header />
+        <div>
+            {/* nội dung ở đây */}
+
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default Service;
