ad7c31bb6f23a686b60be6f38666dda6c5933ae3
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-06 03:57:54 +0700

    update and format path also prepare path for manager/designer/construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFound from "./components/not-found"; // Thêm import cho NotFound
+import NotFound from "./pages/general/not-found"; // Thêm import cho NotFound
 
 // Staff page
+//Consulting
 import ConsultationRequests from "./pages/consulting/consultation-requests";
-import MyProfile from "./pages/my-profile";
 import CreatedOrders from "./pages/consulting/created-orders";
 import OngoingConsultations from "./pages/consulting/ongoing-consultations";
-import OrderDetail from "./pages/order-detail";
-import Order from "./pages/order-detail";
-import HistoryPage from "./pages/customer/history";
-import PriceListStaff from "./pages/price-list-staff";
+//Construction
 
-// Customer page
+//Designer
+
+//Manager
+
+// Home page
 import LoginPage from "./pages/general/login";
 import RegisterPage from "./pages/general/register";
 import HomePage from "./pages/general/homepage";
@@ -22,7 +23,12 @@ import Pricing from "./pages/general/pricing";
 import About from "./pages/general/about";
 import Blog from "./pages/general/blog";
 import ListProject from "./pages/general/list-project";
-
+//General
+import OrderDetail from "./pages/order-detail";
+import Order from "./pages/order-detail";
+import HistoryPage from "./pages/customer/history";
+import PriceListStaff from "./pages/price-list-staff";
+import MyProfile from "./pages/my-profile";
 function App() {
   const router = createBrowserRouter([
     {
@@ -66,18 +72,6 @@ function App() {
       element: <HomePage />,
     },
     {
-      path: "consultation-requests",
-      element: <ConsultationRequests />,
-    },
-    {
-      path: "created-orders",
-      element: <CreatedOrders />,
-    },
-    {
-      path: "ongoing-consultation",
-      element: <OngoingConsultations />,
-    },
-    {
       path: "my-profile",
       element: <MyProfile />,
     },
@@ -96,6 +90,49 @@ function App() {
     {
       path: "price-list",
       element: <PriceListStaff />,
+    },
+    //Consulting-staff
+    {
+      path: "consulting/consultation-requests",
+      element: <ConsultationRequests />,
+    },
+    {
+      path: "/consulting/ongoing-consultation",
+      element: <OngoingConsultations />,
+    },
+    {
+      path: "/consulting/created-orders",
+      element: <CreatedOrders />,
+    },
+    //Construction-staff
+    {
+      path: "/construction/active-project",
+    },
+    {
+      path: "/construction/history-projects",
+    },
+    //Designer
+    {
+      path: "/designer",
+    },
+    //Manager
+    {
+      path: "/manager/design-management",
+    },
+    {
+      path: "/manager/staff-management",
+    },
+    {
+      path: "/manager/quote-management",
+    },
+    {
+      path: "/manager/construction-orders",
+    },
+    {
+      path: "/manager/maintenance-orders",
+    },
+    {
+      path: "/manager/customer-profile-management",
     },
     {
       path: "*", // Route cho các đường dẫn không tồn tại
