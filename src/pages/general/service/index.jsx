7927fac16b4ed5d570046f60bd4ee825da3d2730
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-08 22:51:56 +0700

    create page for service

--- src/pages/general/service/index.jsx
+++ src/pages/general/service/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import Header from "../../../components/header";
-import Footer from "../../../components/footer";
+import CommonTemplate from '../../../components/common-page-template';
+
+const title = 'Dịch vụ';
+const context = 'Trang chủ »  Dịch vụ';
+const banner = 'https://firebasestorage.googleapis.com/v0/b/koi-pond-cons.appspot.com/o/toby-sakata-lJ62jee7oSM-unsplash.jpg?alt=media&token=92c638c4-75d1-4441-932a-f804c3aacd4e';
+
+
 
 function Service() {
   return (
     <div>
-      <Header />
-      <div>{/* nội dung ở đây */}</div>
-      <Footer />
+      <CommonTemplate title={title} context={context} banner={banner}>
+        
+      </CommonTemplate>
     </div>
   );
 }
