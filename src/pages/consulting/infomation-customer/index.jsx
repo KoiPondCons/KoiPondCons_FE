bb37e0427e1bfdb868b953c7451c4512c735a54c
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-10 01:35:18 +0700

    add api

--- src/pages/consulting/infomation-customer/index.jsx
+++ src/pages/consulting/infomation-customer/index.jsx
@@ -1,103 +1,293 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavDashboard from "../../../components/navbar-dashboard";
-import { Button, Col, Form, Row, Select } from "antd";
+import { Button, Col, Form, Row, Select, Table } from "antd";
 import api from "../../../config/axios";
 import "../../../utils/common.css";
 import FormItem from "antd/es/form/FormItem";
+import { useLocation, useParams } from "react-router-dom";
 function InfomationCustomer() {
-  const constructionOrder = {
-    customer_name: "Nguyễn Văn A",
-    customer_phone: "0901234567",
-    customer_email: "nguyenvana@example.com",
-    pond_address: "123 Đường Hoa, Quận 1, TP.HCM",
-    status: "Đang chờ",
-    pond_volume: "1000 m3",
+  const { id } = useParams();
+  const location = useLocation();
+  const actor = location.state;
+  const [listCombo, setListCombo] = useState([]);
+  const [selectedCombo, setSelectedCombo] = useState(null);
+  const [constructionOrder, setConstructionOrder] = useState(null);
+  const [comboConstructionItems, setComboConstructionItems] = useState([]);
+  const [unitPrice, setUnitPrice] = useState();
+  const [promotionList, setPromotionList] = useState([]);
+  const columnsPackage = [
+    {
+      title: "STT",
+      key: "index",
+      render: (text, record, index) => index + 1,
+    },
+    {
+      title: "Đặc tả",
+      dataIndex: "itemContent",
+      key: "itemContent",
+    },
+    {
+      title: "Thời lượng thi công",
+      dataIndex: "duration",
+      key: "duration",
+      width: 200,
+      render: (text, record) => (
+        <span style={{ textAlign: "center" }}>{text} ngày</span>
+      ),
+    },
+  ];
+  const fecthCombo = async () => {
+    try {
+      const response = await api.get("combos");
+      setListCombo(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  const requestContent =
-    "Kính gửi trung tâm, tôi muốn yêu cầu một cuộc kiểm tra hồ cá Koi của mình. Hồ cá của tôi có thể tích khoảng 1000 lít và đang gặp một số vấn đề về nước, tôi nghi ngờ có thể có vi khuẩn hoặc nấm gây bệnh cho cá. Tôi hy vọng các chuyên gia của trung tâm có thể đến kiểm tra và tư vấn giải pháp để cải thiện chất lượng nước. Ngoài ra, tôi cũng muốn biết thêm về các dịch vụ điều trị cá bệnh mà trung tâm cung cấp. Mong sớm nhận được phản hồi từ quý trung tâm. Xin cảm ơn!";
+  const fetchConstructionOrder = async () => {
+    try {
+      const response = await api.get(`orders/${id}`);
+      setConstructionOrder(response.data);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  useEffect(() => {
+    fecthCombo();
+    fetchConstructionOrder();
+  }, []);
+
+  useEffect(() => {
+    if (constructionOrder) {
+      console.log(constructionOrder.data);
+    }
+  }, [constructionOrder]);
+
+  const handleSelectChange = (value) => {
+    setSelectedCombo(value);
+    console.log("Selected combo:", value);
+  };
+
+  const fetchConstructionItems = async () => {
+    try {
+      if (selectedCombo) {
+        const response = await api.get(
+          `comboconstructionitems/combo/${selectedCombo}`
+        );
+        setComboConstructionItems(response.data);
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.log("Bug at fetchConstructionItems, " + error);
+    }
+  };
+  const fetchUnitPrice = async () => {
+    try {
+      const response = await api.get(
+        `comboprices/combo/volume/${selectedCombo}/${constructionOrder.quotation.pondVolume}`
+      );
+      setUnitPrice(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Bug at fetchUnitPrice, " + error);
+    }
+  };
+  const fecthPromotionList = async () => {
+    try {
+      const response = await api.get(
+        `promotions/customer/${constructionOrder.customer.id}`
+      );
+      setPromotionList(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Bug at fecthPromotion, " + error);
+    }
+  };
+  useEffect(() => {
+    fetchConstructionItems();
+    fetchUnitPrice();
+    fecthPromotionList();
+  }, [selectedCombo]);
+  useEffect(() => {
+    if (unitPrice) {
+      console.log(unitPrice.data);
+    }
+  }, [unitPrice]);
+  const isComboSelected = selectedCombo !== null;
+  let totalDiscountPrice = 0;
+  promotionList.forEach((promotion) => {
+    const discountPrice =
+      promotion.discountPercent *
+      unitPrice.unitPrice *
+      constructionOrder.quotation.pondVolume;
+    totalDiscountPrice += discountPrice;
+  });
   return (
-    <NavDashboard actor="consulting">
+    <NavDashboard actor={actor}>
       <h1>THÔNG TIN ĐƠN HÀNG</h1>
       <Form layout="vertical">
         <Row gutter={24}>
           <Col span={8}>
             <label>Họ tên</label>
             <div className="display-input">
-              <span> {constructionOrder.customer_name}</span>
+              <span>
+                {constructionOrder ? constructionOrder.customerName : "N/A"}
+              </span>
             </div>
           </Col>
           <Col span={8}>
             <label>Số điện thoại</label>
             <div className="display-input">
-              <span> {constructionOrder.customer_phone}</span>
+              <span>
+                {constructionOrder ? constructionOrder.customerPhone : "N/A"}
+              </span>
             </div>
           </Col>
           <Col span={8}>
             <label>Email</label>
             <div className="display-input">
-              <span> {constructionOrder.customer_email}</span>
+              <span>
+                {constructionOrder
+                  ? constructionOrder.customer.account.email
+                  : "N/A"}
+              </span>
             </div>
           </Col>
           <Col span={16}>
             <label>Địa chỉ thi công</label>
             <div className="display-input">
-              <span> {constructionOrder.pond_address}</span>
+              <span>
+                {constructionOrder ? constructionOrder.pondAddress : "N/A"}
+              </span>
             </div>
           </Col>
           <Col span={8}>
             <label>Thể tích hồ</label>
             <div className="display-input">
-              <span> {constructionOrder.pond_volume}</span>
+              <span>
+                {constructionOrder && constructionOrder.quotation
+                  ? constructionOrder.quotation.pondVolume
+                  : "N/A"}
+              </span>
             </div>
           </Col>
           <Col span={24}>
             <label>Nội dung khách hàng yêu cầu</label>
             <div className="display-input" style={{ textAlign: "justify" }}>
-              {requestContent}
+              {constructionOrder
+                ? constructionOrder.customerDescription
+                : "N/A"}
             </div>
           </Col>
           <Col span={12}>
             <FormItem
+              name="comboId"
               label="Gói"
               key="packages"
-              rules={[
-                {
-                  required: true,
-                  message: "Vui lòng chọn gói!",
-                },
-              ]}
+              rules={[{ required: true, message: "Vui lòng chọn gói!" }]}
             >
-              <Select placeholder="Chọn gói">
-                <Select.Option value="1">Gói cơ bản</Select.Option>
-                <Select.Option value="2">Gói phổ thông </Select.Option>
-                <Select.Option value="3">Gói cao cấp</Select.Option>
+              <Select placeholder="Chọn gói" onChange={handleSelectChange}>
+                {listCombo.map((combo) => (
+                  <Select.Option key={combo.id} value={combo.id}>
+                    {combo.name}
+                  </Select.Option>
+                ))}
               </Select>
             </FormItem>
           </Col>
           <Col span={12} style={{ display: "flex", alignItems: "center" }}>
             <Button>Tạo bảng báo giá</Button>
           </Col>
-          {/* <Col span={10}>
-            <label>Trạng thái</label>
-            <div className="display-input">
-              <span> {constructionOrder.status}</span>
-            </div>
-          </Col> */}
-          {/* <Col span={7}>
-            <label>Gói</label>
-            <div className="display-input">
-              <span> Trần Kim Nhã</span>
-            </div>
-          </Col>
-          <Col span={7}>
-            <label>Thể tích hồ</label>
-            <div className="display-input">
-              <span> {constructionOrder.pond_volume}</span>
-            </div>
-          </Col> */}
         </Row>
       </Form>
+      {isComboSelected && (
+        <>
+          <div className="price-list-staff-result">
+            <h1>Chi tiết hạng mục</h1>
+            <Table
+              className="table-template"
+              dataSource={comboConstructionItems}
+              columns={columnsPackage}
+              pagination={false}
+            />
+          </div>
+          <div className="container">
+            <div className="card cart">
+              <label className="title">BÁO GIÁ</label>
+              <div className="steps">
+                <div className="step" style={{ maxWidth: "100%" }}>
+                  <div className="payments">
+                    <p>ƯỚC LƯỢNG</p>
+                    <div className="details">
+                      <span style={{ fontWeight: "bold" }}>Đơn giá</span>
+                      <span style={{ textAlign: "right" }}>
+                        <span style={{ textAlign: "right" }}>
+                          {unitPrice
+                            ? `${new Intl.NumberFormat("vi-VN").format(
+                                unitPrice.unitPrice
+                              )} VND/m3`
+                            : "Loading VND/m3"}
+                        </span>
+                      </span>
+                      <span style={{ fontWeight: "bold" }}>Thể tích</span>
+                      <span style={{ textAlign: "right" }}>
+                        {constructionOrder.quotation.pondVolume} m3
+                      </span>
+                    </div>
+                    <hr />
+                    <p style={{ marginTop: "20px" }}>THANH TOÁN</p>
+                    <div className="details">
+                      <span style={{ fontWeight: "bold" }}>Thành tiền</span>
+                      <span style={{ textAlign: "right" }}>
+                        {unitPrice
+                          ? `${new Intl.NumberFormat("vi-VN").format(
+                              unitPrice.unitPrice *
+                                constructionOrder.quotation.pondVolume
+                            )} VND`
+                          : "Loading VND"}
+                      </span>
+
+                      {promotionList.map((promotion) => (
+                        <>
+                          <span style={{ fontWeight: "bold" }}>Giảm giá</span>
+                          <span style={{ textAlign: "right" }}>
+                            {promotion.content}
+                          </span>
+                          <span style={{ fontWeight: "bold" }}></span>
+                          <span style={{ textAlign: "right", gap: "1px" }}>
+                            {new Intl.NumberFormat("vi-VN").format(
+                              promotion.discountPercent *
+                                unitPrice.unitPrice *
+                                constructionOrder.quotation.pondVolume
+                            )}{" "}
+                            VND
+                          </span>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card checkout">
+              <div className="footer">
+                <label className="price">
+                  {unitPrice
+                    ? `${new Intl.NumberFormat("vi-VN").format(
+                        unitPrice.unitPrice *
+                          constructionOrder.quotation.pondVolume -
+                          totalDiscountPrice
+                      )} VND`
+                    : "Loading VND"}
+                </label>
+                <button className="checkout-btn">Gửi báo giá</button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </NavDashboard>
   );
 }
