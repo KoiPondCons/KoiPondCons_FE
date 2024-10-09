0561428432a2dd175255237a0397bee2afea2f46
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-09 15:31:10 +0700

    update path axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://koipondcons-be-0fo9.onrender.com/api/",
   withCredentials: false, // Include credentials with requests
 });
 
