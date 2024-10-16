a824c5c7293dd511d11668326f68abebc86b8b34
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-16 22:13:15 +0700

    create loading page

--- src/pages/general/loading/index.jsx
+++ src/pages/general/loading/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './index.css'
 
 function index() {
   return (
-    <div>index</div>
+    <div className='loader-container'>
+      <div class="loader">
+        <span>KoiPond Team</span>
+        <span>KoiPond Team</span>
+      </div>
+    </div>
   )
 }
 export default index
