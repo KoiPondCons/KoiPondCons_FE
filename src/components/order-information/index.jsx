5c9091feb5a0ec51b4204923658537a228cb2bd9
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-16 15:39:10 +0700

    add field customer description

--- src/components/order-information/index.jsx
+++ src/components/order-information/index.jsx
@@ -1,8 +1,14 @@
-import { Col, Row } from "antd";
+import { Col, Row, Spin } from "antd";
 import React from "react";
 import PropTypes from "prop-types";
 import "../../utils/common.css";
+import { LoadingOutlined } from "@ant-design/icons";
 function OrderInfor({ constructionOrder }) {
+  if (!constructionOrder) {
+    return (
+      <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+    );
+  }
   return (
     <div style={{ margin: "0 3%", backgroundColor: "#FFFFFF" }}>
       <h1 style={{ textAlign: "center", paddingTop: "10px" }}>
@@ -12,13 +18,13 @@ function OrderInfor({ constructionOrder }) {
         <Col span={8}>
           <label>Họ tên</label>
           <div className="display-input">
-            <span> {constructionOrder.customerName}</span>
+            <span> {constructionOrder.customerName || "N/A"}</span>
           </div>
         </Col>
         <Col span={8}>
           <label>Số điện thoại</label>
           <div className="display-input">
-            <span> {constructionOrder.customerPhone}</span>
+            <span> {constructionOrder.customerPhone || "N/A"}</span>
           </div>
         </Col>
         <Col span={8}>
@@ -31,6 +37,12 @@ function OrderInfor({ constructionOrder }) {
           <label>Địa chỉ thi công</label>
           <div className="display-input">
             <span> {constructionOrder.pondAddress}</span>
+          </div>
+        </Col>
+        <Col span={24}>
+          <label>Nội dung khách hàng yêu cầu</label>
+          <div className="display-input" style={{ textAlign: "justify" }}>
+            {constructionOrder ? constructionOrder.customerDescription : "N/A"}
           </div>
         </Col>
         <Col span={10}>
@@ -65,6 +77,7 @@ OrderInfor.propTypes = {
   constructionOrder: PropTypes.shape({
     customerName: PropTypes.string.isRequired,
     customerPhone: PropTypes.string.isRequired,
+    customerDescription: PropTypes.string.isRequired,
     customer: PropTypes.shape({
       account: PropTypes.shape({
         email: PropTypes.string.isRequired,
