bc420b2ac8d7289722160b0f38306a82ed1830e4
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-18 01:17:50 +0700

    Rename and moving loading

---
+++ src/components/loading/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function LoadingPage() {
+  return (
+    <div className="loader-container">
+      <div class="loader">
+        <span>KoiPond Team</span>
+        <span>KoiPond Team</span>
+      </div>
+    </div>
+  );
+}
+export default LoadingPage;
