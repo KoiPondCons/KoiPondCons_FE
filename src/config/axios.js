492941cc22275cbe66f74a0c0e891e637b628923
Tran Kim Nha <nhatkse182290@fpt.edu.vn>
2024-10-19 02:36:39 +0700

    update consultant submit change

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,20 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://koipondcons-be-0fo9.onrender.com/api/",
-//   baseURL: "http://localhost:8080/api/",
+  //   baseURL: "http://localhost:8080/api/",
   withCredentials: false, // Include credentials with requests
 });
 
 const handleBefore = (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 };
 
 const handleError = (error) => {
-    console.log(error);
+  console.log(error);
 };
 
 api.interceptors.request.use(handleBefore, handleError);
