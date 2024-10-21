9f09234cdb2b596b1cc94288431abe27613306ed
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-21 16:53:32 +0700

    Create component progressConstruction , using this for construction-order-detail, and progress-order-customer

---
+++ src/pages/customer/progress-construction-customer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CommonPageTemplate from "../../../components/common-page-template";
+import ProgressConstruction from "../../../components/progress-construction";
+
+function ProgressConstructionCustomer() {
+  return (
+    <CommonPageTemplate>
+      <ProgressConstruction />
+    </CommonPageTemplate>
+  );
+}
+
+export default ProgressConstructionCustomer;
