2de7617a6be8ffda6a8e791ce766703585af5b98
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-23 13:07:05 +0700

    add banner

--- src/pages/customer/progress-construction-customer/index.jsx
+++ src/pages/customer/progress-construction-customer/index.jsx
@@ -3,8 +3,11 @@ import CommonPageTemplate from "../../../components/common-page-template";
 import ProgressConstruction from "../../../components/progress-construction";
 
 function ProgressConstructionCustomer() {
+  const banner =
+    "https://firebasestorage.googleapis.com/v0/b/koi-pond-cons.appspot.com/o/banner-design-review.avif?alt=media&token=28f3cafe-4c84-4ea7-8fc9-f918d72c9919";
+  const title = "Chi tiết tiến độ";
   return (
-    <CommonPageTemplate>
+    <CommonPageTemplate banner={banner} title={title}>
       <ProgressConstruction />
     </CommonPageTemplate>
   );
