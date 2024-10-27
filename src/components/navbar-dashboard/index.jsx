146c31ab8a3c45bc0d413764092ae2f1ae211cf9
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-27 16:35:23 +0700

    remove 2 path of manager unnecessary

--- src/components/navbar-dashboard/index.jsx
+++ src/components/navbar-dashboard/index.jsx
@@ -6,14 +6,10 @@ import {
   SettingOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import {
-  Link,
-  useNavigate,
-} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import IMGDashboard from "../../images/Dashboard.png"; // Nhập hình ảnh
 
 const NavDashboard = ({ children, actor }) => {
-
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -54,9 +50,7 @@ const NavDashboard = ({ children, actor }) => {
     ],
     manager: [
       { path: "/manager", label: "Báo cáo doanh thu" },
-      { path: "/manager/design-management", label: "Quản lý mẫu thiết kế" },
       { path: "/manager/staff-management", label: "Quản lý nhân sự" },
-      { path: "/manager/quote-management", label: "Quản lý báo giá" },
       { path: "/manager/construction-orders", label: "Đơn hàng thi công" },
       { path: "/manager/maintenance-orders", label: "Đơn hàng bảo dưỡng" },
       {
@@ -80,7 +74,10 @@ const NavDashboard = ({ children, actor }) => {
           <ul>
             {links[actor]?.map((link, index) => (
               <li key={index}>
-                <Link to={link.path} style={{ textDecoration: "none" }}> {link.label}</Link>
+                <Link to={link.path} style={{ textDecoration: "none" }}>
+                  {" "}
+                  {link.label}
+                </Link>
                 {/* <a href={link.path}>
                   {link.label}
                 </a> */}
