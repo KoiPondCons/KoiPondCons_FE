f86ac0f78ef6121a6ea0b18def8bf70cef55d145
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-29 23:49:37 +0700

    Remove table

--- src/pages/manager/report/index.jsx
+++ src/pages/manager/report/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import TableTemplate from "../../../components/table";
+import NavDashboard from "../../../components/navbar-dashboard";
 
 function Report() {
   const title = "Báo cáo doanh thu";
   return (
     <div>
-      <TableTemplate title={title} actor="manager"></TableTemplate>
+      <NavDashboard actor="manager">
+        <h1 style={{ textAlign: "center" }}>{title}</h1>
+      </NavDashboard>
     </div>
   );
 }
