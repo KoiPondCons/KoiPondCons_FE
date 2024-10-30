7c93d94b965374c1c4a99312a2509080d077beef
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-30 13:01:14 +0700

    modify dashboard

--- src/components/navbar-dashboard/index.jsx
+++ src/components/navbar-dashboard/index.jsx
@@ -6,117 +6,184 @@ import {
   SettingOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import IMGDashboard from "../../images/Dashboard.png"; // Nhập hình ảnh
 import Header from "../header";
 import Footer from "../footer";
+import { Menu } from "antd";
 
 const NavDashboard = ({ children, actor }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [items, setItems] = useState();
   useEffect(() => {
     if (token !== null) {
       setIsLoggedIn(true);
     }
   }, [token]);
-
+  useEffect(() => {
+    switch (actor) {
+      case "manager":
+        setItems(itemsManager);
+        break;
+      case "consulting":
+        setItems(itemsConsulting);
+        break;
+      case "construction":
+        setItems(itemsConstruction);
+        break;
+      case "designer":
+        setItems(itemsDesigner);
+        break;
+      case "customer":
+        return (
+          <div>
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        );
+    }
+  }, [actor]);
   const handleLogout = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("token");
     navigate("/");
   };
 
-  const links = {
-    consulting: [
-      {
-        path: "/consulting",
-        label: "Khách hàng cần tư vấn",
-      },
-      {
-        path: "/consulting/ongoing-consultation",
-        label: "Khách hàng đang tư vấn",
-      },
-      { path: "/consulting/created-orders", label: "Đơn hàng đã tạo" },
-      { path: "/my-profile", label: "Hồ sơ của tôi" },
-    ],
-    construction: [
-      { path: "/construction", label: "Dự án đang thực hiện" },
-      { path: "/construction/history-construction", label: "Lịch sử dự án" },
-      { path: "/my-profile", label: "Hồ sơ của tôi" },
-    ],
-    designer: [
-      { path: "/designer", label: "Xem đơn thiết kế" },
-      { path: "/my-profile", label: "Hồ sơ của tôi" },
-    ],
-    manager: [
-      { path: "/manager", label: "Báo cáo doanh thu" },
-      { path: "/manager/staff-management", label: "Quản lý nhân sự" },
-      { path: "/manager/construction-orders", label: "Đơn hàng thi công" },
-      { path: "/manager/maintenance-orders", label: "Đơn hàng bảo dưỡng" },
-      {
-        path: "/manager/customer-profile-management",
-        label: "Quản lý hồ sơ khách hàng",
-      },
-      { path: "/my-profile", label: "Hồ sơ của tôi" },
-    ],
-  };
-  if (actor == "customer")
-    return (
-      <div>
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    );
-  else {
-    return (
-      <div>
-        <div className="authen-template-dashboard">
-          <div className="sidebar">
-            <a href="">
-              <img
-                className="dashboard"
-                src={IMGDashboard} // Sử dụng biến đã nhập
-                alt=""
-              />
-            </a>
-            <ul>
-              {links[actor]?.map((link, index) => (
-                <li key={index}>
-                  <Link to={link.path} style={{ textDecoration: "none" }}>
-                    {" "}
-                    {link.label}
-                  </Link>
-                  {/* <a href={link.path}>
-                  {link.label}
-                </a> */}
-                </li>
-              ))}
-            </ul>
-            <div></div>
-          </div>
+  const itemsManager = [
+    {
+      key: "/manager",
+      label: <Link to="/manager">Báo cáo doanh thu</Link>,
+    },
+    {
+      key: "manage-order",
+      label: "Quản lý đơn hàng",
+      children: [
+        {
+          key: "/manager/construction-orders",
+          label: <Link to="/manager/construction-orders">Thi công</Link>,
+        },
+        {
+          key: "/manager/maintenance-orders",
+          label: <Link to="/manager/maintenance-orders">Dịch vụ</Link>,
+        },
+      ],
+    },
+    {
+      key: "manager-account",
+      label: "Quản lý hồ sơ",
+      children: [
+        {
+          key: "/manager/staff-management",
+          label: <Link to="/manager/staff-management">Nhân viên</Link>,
+        },
+        {
+          key: "/manager/customer-profile-management",
+          label: (
+            <Link to="/manager/customer-profile-management">Khách hàng</Link>
+          ),
+        },
+      ],
+    },
+    {
+      key: "manage-price",
+      label: "Quản lý báo giá",
+      children: [
+        {
+          key: "/manager/promotion",
+          label: <Link to="/manager/promotion">Khuyến mãi</Link>,
+        },
+        {
+          key: "/manager/combo",
+          label: <Link to="/manager/combo">Gói</Link>,
+        },
+      ],
+    },
+    {
+      key: "/manager/blog",
+      label: <Link to="/manager/blog">Quản lý blog</Link>,
+    },
+  ];
+  const itemsConsulting = [
+    {
+      key: "/consulting",
+      label: <Link to="/consulting">Khách hàng cần tư vấn</Link>,
+    },
+    {
+      key: "/consulting/ongoing-consultation",
+      label: (
+        <Link to="/consulting/ongoing-consultation">
+          Khách hàng đang tư vấn
+        </Link>
+      ),
+    },
+  ];
+  const itemsConstruction = [
+    {
+      key: "/construction",
+      label: <Link to="/construction">Dự án đang thực hiện</Link>,
+    },
+    {
+      key: "/construction/history-construction",
+      label: <Link to="/construction/history-construction">Lịch sử dự án</Link>,
+    },
+    // {
+    //   key: "/my-profile",
+    //   label: <Link to="/my-profile">Hồ sơ của tôi</Link>,
+    // },
+  ];
+  const itemsDesigner = [
+    {
+      key: "/designer",
+      label: <Link to="/designer">Xem đơn thiết kế</Link>,
+    },
+    // {
+    //   key: "/my-profile",
+    //   label: <Link to="/my-profile">Hồ sơ của tôi</Link>,
+    // },
+  ];
 
-          <div className="content">
-            <div className="navbar-admin">
-              <div className="menu-icon">☰</div>
-              <div className="greeting">Xin chào, admin!</div>
-              <div className="icons">
-                <BellOutlined className="icon noti" />
-                <SettingOutlined className="icon setting" />
-                <LogoutOutlined
-                  className="icon logout"
-                  onClick={handleLogout}
-                />
-              </div>
+  return (
+    <div>
+      <div className="authen-template-dashboard">
+        <div className="sidebar">
+          <a href="">
+            <img
+              className="dashboard"
+              src={IMGDashboard} // Sử dụng biến đã nhập
+              alt=""
+            />
+          </a>
+          <Menu
+            style={{ width: 256 }}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+            items={items}
+            selectedKeys={currentPath}
+          />
+          <div></div>
+        </div>
+
+        <div className="content">
+          <div className="navbar-admin">
+            <div className="menu-icon">☰</div>
+            <div className="greeting">Xin chào, admin!</div>
+            <div className="icons">
+              <BellOutlined className="icon noti" />
+              <SettingOutlined className="icon setting" />
+              <LogoutOutlined className="icon logout" onClick={handleLogout} />
             </div>
-            {children}
           </div>
+          {children}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default NavDashboard;
