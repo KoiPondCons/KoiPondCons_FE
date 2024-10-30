61e5c5ae37eb7cd95065a543d68ab1218cc80f96
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-10-30 16:10:01 +0700

    rename folder

--- src/pages/manager/config-price/index.jsx
+++ src/pages/manager/config-price/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import NavbarDashboard from '../../../../components/navbar-dashboard'
-import { ConfigProvider, Table, InputNumber } from 'antd'
-import { useState, useEffect } from 'react';
-import api from '../../../../config/axios';
+import React from "react";
+import NavbarDashboard from "../../../components/navbar-dashboard";
+import { ConfigProvider, Table, InputNumber } from "antd";
+import { useState, useEffect } from "react";
+import api from "../../../config/axios";
 
 function ConfigPrice() {
   const [dataSource, setDataSource] = useState([]);
@@ -19,8 +19,8 @@ function ConfigPrice() {
         const responsePremium = await api.get(`comboprices/combo/${premiumId}`); // API cho Gói cao cấp
         const formattedData = responseBasic.data.map((item, index) => ({
           basic: item.unitPrice, // Gói cơ bản
-          popular: responsePopular.data[index]?.unitPrice || 'N/A', // Gói phổ thông
-          premium: responsePremium.data[index]?.unitPrice || 'N/A', // Gói cao cấp
+          popular: responsePopular.data[index]?.unitPrice || "N/A", // Gói phổ thông
+          premium: responsePremium.data[index]?.unitPrice || "N/A", // Gói cao cấp
         }));
         setDataSource(formattedData);
         console.log(formattedData);
@@ -55,82 +55,80 @@ function ConfigPrice() {
 
   const columns = [
     {
-      title: 'Kích thước',
-      dataIndex: 'size',
-      key: 'size',
+      title: "Kích thước",
+      dataIndex: "size",
+      key: "size",
       render: (_, __, index) => sizes[index],
-      align: 'center',
+      align: "center",
     },
     {
-      title: 'Gói cơ bản',
-      dataIndex: 'basic',
-      key: 'basic',
+      title: "Gói cơ bản",
+      dataIndex: "basic",
+      key: "basic",
       render: (text, record) => (
         <InputNumber
           defaultValue={text}
           onChange={(value) => handlePriceChange(basicId, value)} // Cập nhật giá khi thay đổi
         />
       ),
-      align: 'center',
+      align: "center",
     },
     {
-      title: 'Gói phổ thông',
-      dataIndex: 'popular',
-      key: 'popular',
+      title: "Gói phổ thông",
+      dataIndex: "popular",
+      key: "popular",
       render: (text, record) => (
         <InputNumber
           defaultValue={text}
           onChange={(value) => handlePriceChange(popularId, value)} // Cập nhật giá khi thay đổi
         />
       ),
-      align: 'center',
+      align: "center",
     },
     {
-      title: 'Gói cao cấp',
-      dataIndex: 'premium',
-      key: 'premium',
+      title: "Gói cao cấp",
+      dataIndex: "premium",
+      key: "premium",
       render: (text, record) => (
         <InputNumber
           defaultValue={text}
           onChange={(value) => handlePriceChange(premiumId, value)} // Cập nhật giá khi thay đổi
         />
       ),
-      align: 'center',
+      align: "center",
     },
   ];
   const sizes = [
-    '8 - 10m3',
-    '10 - 20m3',
-    '20 - 50m3',
-    '50 - 100m3',
-    '100m3 trở lên',
+    "8 - 10m3",
+    "10 - 20m3",
+    "20 - 50m3",
+    "50 - 100m3",
+    "100m3 trở lên",
   ];
   return (
-      <NavbarDashboard
-        actor="manager"
-      >
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <ConfigProvider
-            theme={{
-              components: {
-                Table: {
-                  fontSize: 20,
-                },
+    <NavbarDashboard actor="manager">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ConfigProvider
+          theme={{
+            components: {
+              Table: {
+                fontSize: 20,
               },
-            }}
-          >
-            <Table
-              columns={columns}
-              dataSource={dataSource}
-              style={{ background: "white", margin: "0px 40px" }}
-              pagination={false}
-            />
-          </ConfigProvider>
-        )}
-      </NavbarDashboard>
-  )
+            },
+          }}
+        >
+          <Table
+            columns={columns}
+            dataSource={dataSource}
+            style={{ background: "white", margin: "0px 40px" }}
+            pagination={false}
+          />
+        </ConfigProvider>
+      )}
+    </NavbarDashboard>
+  );
 }
 
 export default ConfigPrice;
