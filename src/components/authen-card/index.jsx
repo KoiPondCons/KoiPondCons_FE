05d2f26385c99f4c54bd9b18a1feb02b3551e51f
ngdungteddy <nguyentrungdung1478@gmail.com>
2024-10-31 17:46:45 +0700

    create post

--- src/components/authen-card/index.jsx
+++ src/components/authen-card/index.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Pagination } from 'antd';
 import './index.css';
 import { Link } from 'react-router-dom'; // Thêm import này
+import { createClient } from "contentful";
 
 function AuthenCardList({ cardData }) { // Nhận cardData từ props
   const [currentPage, setCurrentPage] = useState(1);
-  const cardsPerPage = 9; // Số lượng thẻ trên mỗi trang
+  const cardsPerPage = 1; // Số lượng thẻ trên mỗi trang
 
   // Tính toán chỉ số của các thẻ cần hiển thị
   const indexOfLastCard = currentPage * cardsPerPage;
@@ -15,33 +16,43 @@ function AuthenCardList({ cardData }) { // Nhận cardData từ props
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
+  const client = createClient({ space: "7cnffzqr25eq", accessToken: "S0LVPWr1CNSz3SyvHQUsOjgsXS_L1a2V8uV7z8LzrNU" });
+  const [blogPosts, setBlogPosts] = useState([]);
+  useEffect(() => {
+    const getAllBlogPosts = async () => {
+      try {
+        const response = await client.getEntries().then(entries => {
+          console.log(entries);
+          setBlogPosts(entries);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllBlogPosts();
+  }, []);
 
   return (
-    <div className='authen-card-background'>
-      <div>
-        <div className='authen-card-container' style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-          {currentCards.map(card => (
-            <div key={card.id} className='authen-card-item' style={{ width: '27%' }}>
-              <Link to={`/article/${card.id}`} style={{ textDecoration: 'none' }}>
-                <Card bordered={false} className='authen-card'>
-                  <img src={card.imageUrl} alt={card.title} className='authen-card-image' style={{ width: '100%', height: '400px', objectFit: 'cover' }} />
-                  <div className='authen-card-content'>
-                    <h3 className='authen-card-title' style={{ textAlign: 'center', margin: 0 }}>{card.title}</h3>
-                    <p className='authen-card-description' style={{ margin: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: '100%' }}>{card.description}</p>
-                  </div>
-                </Card>
-              </Link>
+    <div>
+      {blogPosts?.items?.map((post) => (
+        <div className='authen-card-background post' key={post.sys.id}>
+            <div className='authen-card-container' style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+              {currentCards.map(card => (
+                <div key={card.id} className='authen-card-item' style={{ width: '27%' }}>
+                  <Link to={`/article/${post.sys.id}`} style={{ textDecoration: 'none' }}>
+                    <Card bordered={false} className='authen-card'>
+                      <img src={post.fields.image.fields.file.url} alt={post.fields.title} className='authen-card-image' style={{ width: '100%', height: '400px', objectFit: 'cover' }} />
+                      <div className='authen-card-content'>
+                        <h3 className='authen-card-title' style={{ textAlign: 'center', margin: 0 }}>{post.fields.tittle}</h3>
+                        <p className='authen-card-description' style={{ margin: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: '100%' }}>{post.fields.blog}</p>
+                      </div>
+                    </Card>
+                  </Link>
+                </div>
+              ))}
             </div>
-          ))}
         </div>
-        <Pagination
-          current={currentPage}
-          pageSize={cardsPerPage}
-          total={cardData.length}
-          onChange={handlePageChange}
-          showSizeChanger={false}
-        />
-      </div>
+      ))}
     </div>
   );
 }
--- src/components/blog-content/index.jsx
+++ src/components/blog-content/index.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 import Header from '../header'
 import Footer from '../footer'
 import './index.css'
+import { createClient } from "contentful";
+
 function BlogContent() {
+    const client = createClient({ space: "7cnffzqr25eq", accessToken: "S0LVPWr1CNSz3SyvHQUsOjgsXS_L1a2V8uV7z8LzrNU" });
+    const { id } = useParams();
+    const [singleBlog, setSingleBlog] = useState([]);
+    console.log(id);
+    useEffect(() => {
+        const getBlogContent = async () => {
+            try {
+                await client.getEntry(id).then((entry) => {
+                    setSingleBlog(entry);
+                    console.log(entry);
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getBlogContent();
+    }, []);
     return (
         <div>
             <Header />
-            <div className='blog-content-body' style={{ background: "white" , width: "100%",height: "100vh", margin: "0 auto", padding: " 2% 20%"}}>
-                <h1>Blog Content</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            <div className='blog-content-body' style={{ background: "white", width: "100%", margin: "0 auto", padding: " 2% 20%" }}>
+                <h1>{singleBlog?.fields?.tittle}</h1>
+                <p>{singleBlog?.fields?.blog}</p>
+                <img src={singleBlog?.fields?.image?.fields?.file?.url} alt={singleBlog?.fields?.tittle} />
             </div>
             <Footer />
         </div>
