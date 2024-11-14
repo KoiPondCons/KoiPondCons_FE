b9f92d39af9996d6c01e80b1836b114334849c52
Khoa Vo <khoavnase183164@fpt.edu.vn>
2024-11-14 07:49:54 +0700

    modify verify account,  fix logic

--- src/pages/general/verify/index.jsx
+++ src/pages/general/verify/index.jsx
@@ -1,71 +1,71 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../../../components/header";
 import Footer from "../../../components/footer";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import api from "../../../config/axios";
 function verifyEmail1() {
+  const location = useLocation();
 
-    const location = useLocation();
+  const ref = useRef(false);
 
-    const ref = useRef(false);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    // Retrieve 'accountId' parameter from the query string
+    const queryParams = new URLSearchParams(location.search);
+    const accountId = queryParams.get("accountId");
 
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        // Retrieve 'accountId' parameter from the query string
-        const queryParams = new URLSearchParams(location.search);
-        const accountId = queryParams.get('accountId');
-
-        
-
-        const verifyAccountResponse = async () => {
-            try {
-                const response = await api.put(`verify-account/${accountId}`);
-                // message.success('Payment response sent successfully!'); // Show success message
-                console.log('Response from server:', response.data);
-                switch(response.data.status) {
-                    case 1:
-                        setMessage("Đã xác thực tài khoản thành công!");
-                        break;
-                    case 2:
-                        setMessage("Tài khoản này đã được xác thực trước đó!");
-                        break;
-                    case 3:
-                        setMessage("Xảy ra lỗi khi xác thực!");
-                        break;
-
-                }
-            } catch (error) {
-                // message.error('Failed to send payment response. Please try again.');
-                console.error('Error:', error);
-            }
-            console.log(accountId);
-            setStatus(accountId);
-        };
-
-        if (ref.current) {
-            verifyAccountResponse();
-        } else {
-            ref.current = true;
+    const verifyAccountResponse = async () => {
+      try {
+        const response = await api.put(`verify-account/${accountId}`);
+        // message.success('Payment response sent successfully!'); // Show success message
+        console.log("Response from server:", response.data);
+        switch (response.data.status) {
+          case 1:
+            setMessage("Đã xác thực tài khoản thành công!");
+            break;
+          case 2:
+            setMessage("Tài khoản này đã được xác thực trước đó!");
+            break;
+          case 3:
+            setMessage("Xảy ra lỗi khi xác thực!");
+            break;
         }
-    }, []);
+      } catch (error) {
+        // message.error('Failed to send payment response. Please try again.');
+        console.error("Error:", error);
+      }
+      console.log(accountId);
+      setStatus(accountId);
+    };
 
+    verifyAccountResponse();
+    // if (ref.current) {
+    //   verifyAccountResponse();
+    // } else {
+    //   ref.current = true;
+    // }
+  }, []);
 
-
-    return (
-        <div>
-            {/* <Header /> */}
-            <div 
-                style={{
-                    backgroundColor: "white",
-                }}
-            >
-                <h1>{message}</h1>
-                <button style={{backgroundColor: "#23c483", color: "white"}} onClick={() => navigate('/homepage')}>VỀ TRANG CHỦ</button>
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      {/* <Header /> */}
+      <div
+        style={{
+          backgroundColor: "white",
+        }}
+      >
+        <h1>{message}</h1>
+        <button
+          style={{ backgroundColor: "#23c483", color: "white" }}
+          onClick={() => navigate("/homepage")}
+        >
+          VỀ TRANG CHỦ
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default verifyEmail1
+export default verifyEmail1;
